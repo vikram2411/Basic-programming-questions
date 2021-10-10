601671b8f758a9bd218621a6bbcf268239dbb4ab
vikram2411 <79372413+vikram2411@users.noreply.github.com>
2021-10-10 14:06:19 +0530

    Create missing_number.js

---
+++ missing_number.js
@@ -0,0 +1,10 @@
+var a = [100],
+  count = 100;
+var missing = new Array();
+
+for (var i = 1; i <= count; i++) {
+  if (a.indexOf(i) == -1) {
+    missing.push(i);
+  }
+}
+console.log(missing); // to check the result.
